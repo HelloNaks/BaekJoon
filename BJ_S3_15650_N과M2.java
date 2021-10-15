9d89679190f8d01aa6514a1528095a46bdc8079c
HelloNaks <49478141+HelloNaks@users.noreply.github.com>
2021-10-15 19:59:40 +0900

    백트래킹 문제입니다.

---
+++ BJ_S3_15650_N과M2.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class Main {
+
+	static int N, M;
+	static int[] path=new int[9];
+	static boolean[] v=new boolean[9];
+	public static void main(String[] args) {
+		/*
+		 * 1. 아이디어
+		 * 백트래킹 재귀함수 안에서, for 돌면서 숫자 선택(이때 방문여부 확인)
+		 * 재귀함수에서 M개를 선택할경우 print
+		 * 2. 시간복잡도
+		 * N! -> 가능
+		 * 3. 자료구조
+		 * 결과값 저장 int[]
+		 * 방문여부 체크 bool[]
+		 */
+		Scanner scann = new Scanner(System.in);
+		N=scann.nextInt();
+		M=scann.nextInt();
+		run(0,1);
+	}
+	private static void print() {
+		for (int i = 0; i < M; i++) {
+			System.out.print(path[i]+" ");
+		}
+		System.out.println();
+	}
+	private static void run(int lev, int start) {
+		if(lev==M) {
+			print();
+			return;
+		}
+		for (int i = start; i <= N; i++) {
+			if(v[i]) continue;
+			v[i]=true;
+			path[lev]=i;
+			run(lev+1,i);
+			v[i]=false;
+		}
+	}
+
+	
+}
