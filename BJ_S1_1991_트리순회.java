3986b2a599dd9b76c2cebd92e229fe550f827a38
HelloNaks <49478141+HelloNaks@users.noreply.github.com>
2021-12-17 12:57:11 +0900

    트리 문제입니다.

--- BJ_S1_1991_트리순회.java
+++ BJ_S1_1991_트리순회.java
@@ -1,78 +1,87 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
 
-	public static void main(String[] args){
-		Scanner scann = new Scanner(System.in);
-		
-		int N=scann.nextInt();
+	static int N;
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		N = Integer.parseInt(br.readLine());
 		Tree tree = new Tree();
 		for (int i = 0; i < N; i++) {
-			tree.Add(scann.next().charAt(0),scann.next().charAt(0),scann.next().charAt(0));
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			char c = st.nextToken().toCharArray()[0];
+			char l = st.nextToken().toCharArray()[0];
+			char r = st.nextToken().toCharArray()[0];
+			tree.add(c, l, r);
 		}
-		
-		tree.Preorder(tree.root);
+		tree.preOrder(tree.root);
 		System.out.println();
-		tree.Inorder(tree.root);
+		tree.inOrder(tree.root);
 		System.out.println();
-		tree.Postorder(tree.root);
+		tree.postOrder(tree.root);
+		System.out.println();
 	}
 
-	public static class Tree{
+	public static class Tree {
 		Node root;
-		
-		public void Add(char c, char left, char right) {
+
+		public void add(char c, char l, char r) {
 			if(root == null) {
-				if(c!='.') root = new Node(c);
-				if(left!='.') root.left = new Node(left);
-				if(right!='.') root.right = new Node(right);
+				if(c != '.') root = new Node(c);
+				if(l != '.') root.left = new Node(l);
+				if(r != '.') root.right = new Node(r);
 			}else {
-				Search(root,c,left,right);
+				search(root,c,l,r);
+			}
+		}
+		
+		private void search(Node rot, char c, char l, char r) {
+			if(rot==null) return;
+			if(rot.c==c) {
+				rot.left=new Node(l);
+				rot.right=new Node(r);
+			}
+			else {
+				search(rot.left,c,l,r);
+				search(rot.right,c,l,r);
 			}
 		}
 
-		//후위
-		public void Postorder(Node root) {
-
-			if(root.left!=null) Postorder(root.left);
-			if(root.right!=null) Postorder(root.right);
-			System.out.print(root.c);
+		public void preOrder(Node r) {
+			if(r.c!='.')
+				System.out.print(r.c);
+			if(r.left!=null) preOrder(r.left);
+			if(r.right!=null) preOrder(r.right);
 		}
 
-		//중위
-		public void Inorder(Node root) {
-			if(root.left!=null) Inorder(root.left);
-			System.out.print(root.c);
-			if(root.right!=null) Inorder(root.right);
+		public void inOrder(Node r) {
+			if(r.left!=null) inOrder(r.left);
+			if(r.c!='.')
+				System.out.print(r.c);
+			if(r.right!=null) inOrder(r.right);
 		}
 
-		//전위
-		public void Preorder(Node root) {
-			System.out.print(root.c);
-			if(root.left!=null) Preorder(root.left);
-			if(root.right!=null) Preorder(root.right);
-		}
-
-		private void Search(Node root, char c, char left, char right) {
-			if(root==null) return;
-			else if(root.c==c) {
-				if(left!='.') root.left = new Node(left);
-				if(right!='.') root.right = new Node(right); 
-			}else {
-				Search(root.left,c,left,right);
-				Search(root.right,c,left,right);
-			}
+		public void postOrder(Node r) {
+			if(r.left!=null) postOrder(r.left);
+			if(r.right!=null) postOrder(r.right);
+			if(r.c!='.')
+				System.out.print(r.c);
 		}
 	}
 
-	public static class Node{
+	public static class Node {
 		char c;
 		Node left;
 		Node right;
-		
+
 		public Node(char c) {
 			super();
 			this.c = c;
 		}
 	}
+
 }
