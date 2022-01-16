3d98421a6de7aeaef9521e47366d1b03b5256d3c
HelloNaks <49478141+HelloNaks@users.noreply.github.com>
2022-01-17 00:50:50 +0900

    수학 문제입니다.

---
+++ BJ_B3_11022_A+B-8.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner scann = new Scanner(System.in);
+		int N=scann.nextInt();
+		for(int i=1;i<=N;i++) {
+			int a=scann.nextInt();
+			int b=scann.nextInt();
+			System.out.println("Case #"+i+": "+a+" + "+b+" = "+(a+b));
+		}
+	}
+}
