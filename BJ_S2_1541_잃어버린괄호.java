c3e9409b21ad12616b9bd79e52bf90bf6c5f7b02
HelloNaks <49478141+HelloNaks@users.noreply.github.com>
2022-01-21 16:30:37 +0900

    feat: 수학 / 문자열 / 그리디 알고리즘 / 파싱

---
+++ BJ_S2_1541_잃어버린괄호.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(),"-");
+		int sum=Integer.MAX_VALUE;
+		while(st.hasMoreTokens()) {
+			int temp=0;
+			
+			StringTokenizer add = new StringTokenizer(st.nextToken(), "+");
+			while(add.hasMoreTokens()) {
+				temp+=Integer.parseInt(add.nextToken());
+			}
+			if(sum==Integer.MAX_VALUE) {
+				sum=temp;
+			}else {
+				sum-=temp;
+			}
+		}
+		System.out.println(sum);
+	}
+}
