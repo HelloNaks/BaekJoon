2fe3d8089763ddde83d7d488193eca1fa525a4c6
김낙현 <49478141+HelloNaks@users.noreply.github.com>
2022-06-14 00:12:01 +0900

    feat: 구현

---
+++ BJ_B5_2753_윤년.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner scann = new Scanner(System.in);
+		int N=scann.nextInt();
+		if((N%4==0 && N%100!=0) || N%400==0){
+			System.out.println("1");
+		}else {
+			System.out.println("0");
+		}
+	}
+
+}
