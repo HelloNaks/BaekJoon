032b34ae0b712e8402aae440fbca844842f85526
김낙현 <49478141+HelloNaks@users.noreply.github.com>
2022-06-14 00:14:45 +0900

    feat: 수학

---
+++ BJ_B5_2420_사파리월드.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner scann = new Scanner(System.in);
+		long N=scann.nextLong();
+		long M=scann.nextLong();
+		System.out.println(Math.abs(N-M));
+	}
+
+}
