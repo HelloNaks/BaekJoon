31d31fcd42a6e002bcb1836bc90c9b485b3c4567
김낙현 <49478141+HelloNaks@users.noreply.github.com>
2022-06-13 21:04:14 +0900

    feat: 구현

---
+++ BJ_B5_11382_꼬마정민.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner scann = new Scanner(System.in);
+		long a=scann.nextLong();
+		long b=scann.nextLong();
+		long c=scann.nextLong();
+		System.out.println(a+b+c);
+	}
+
+}
