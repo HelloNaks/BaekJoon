c31df462360b4b2d09ded63970c975a5cc28b130
김낙현 <49478141+HelloNaks@users.noreply.github.com>
2024-10-30 11:23:55 +0900

    Create BJ11722.java

---
+++ 백준/Java/Silver/BJ11722.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+public class BJ11722 {
+    public static void main(String[] args){
+        Scanner sc= new Scanner(System.in);
+        int N = sc.nextInt();
+        int[] arr = new int[N];
+        for(int i=0;i<N;i++){
+            arr[i] = sc.nextInt();
+        }
+        System.out.println(LDS(arr));
+    }
+
+    static int LDS(int[] arr){
+        int ret = 0;
+        List<Integer> list = new LinkedList<>();
+        list.add(arr[0]);
+
+        for(int i=1;i<arr.length;i++){
+            if(arr[i] < list.get(list.size()-1)){
+                list.add(arr[i]);
+                ret = list.size() - 1;
+            }
+            int pos = binary_search(list, 0, ret, arr[i]);
+            list.set(pos, arr[i]);
+        }
+        return ret + 1;
+    }
+
+    static int binary_search(List<Integer> list, int st, int end, int val){
+        while(st < end){
+            int mid = (st+end)/2;
+            if(val < list.get(mid)) st = mid+1;
+            else end = mid;
+        }
+        return end;
+    }
+}
