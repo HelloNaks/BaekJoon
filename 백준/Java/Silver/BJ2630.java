f43d19921ceeeb3fa901293be0745ac8ef139482
김낙현 <49478141+HelloNaks@users.noreply.github.com>
2024-11-01 21:02:01 +0900

    Create BJ2630.java

---
+++ 백준/Java/Silver/BJ2630.java
@@ -0,0 +1,64 @@
+import java.util.*;
+
+public class BJ2630 {
+    static int white = 0, blue = 0;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int[][] arr = new int[N][N];
+        for(int i=0;i<N;i++){
+            for(int j=0;j<N;j++){
+                arr[i][j] = sc.nextInt();
+            }
+        }
+        int depth = 0;
+        if(N==2){
+            depth = 1;
+        }else if(N==4){
+            depth = 2;
+        }else if(N==8){
+            depth = 3;
+        }else if(N==16){
+            depth = 4;
+        }else if(N==32){
+            depth = 5;
+        }else if(N==64){
+            depth = 6;
+        }else if(N==128){
+            depth = 7;
+        }
+
+        run(arr, 0,N,0,N);
+
+        System.out.println(white);
+        System.out.println(blue);
+    }
+
+    static void run(int[][] arr, int r1, int r2, int c1, int c2){
+        boolean flag = false;
+        int temp = arr[r1][c1];
+        for(int i=r1;i<r2;i++){
+            for(int j=c1;j<c2;j++){
+                if(arr[i][j] != temp){
+                    flag=true;
+                }
+            }
+            if(flag){
+                break;
+            }
+        }
+        if(!flag){
+            if(temp==1){
+                blue++;
+            }else{
+                white++;
+            }
+        }else{
+            run(arr,r1,(r1+r2)/2,c1,(c1+c2)/2);
+            run(arr,r1,(r1+r2)/2,(c1+c2)/2,c2);
+            run(arr,(r1+r2)/2,r2,c1,(c1+c2)/2);
+            run(arr,(r1+r2)/2,r2,(c1+c2)/2,c2);
+        }
+    }
+}
