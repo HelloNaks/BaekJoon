1464c8553ccea90d04880d46063c268cb0ef5853
김낙현 <49478141+HelloNaks@users.noreply.github.com>
2024-11-08 20:48:13 +0900

    Create BJ12738.java

---
+++ 백준/Java/Gold/BJ12738.java
@@ -0,0 +1,42 @@
+import java.util.*;
+public class BJ12738 {
+    static int[] arr;
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        arr = new int[N];
+        for(int i=0;i<N;i++){
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.println(LIS(arr));
+    }
+
+    static int LIS(int[] arr){
+        int ret = 0;
+        List<Integer> list = new ArrayList<>();
+        list.add(arr[0]);
+
+        for(int i=1;i<arr.length;i++){
+            if(arr[i] > list.get(list.size()-1)){
+                list.add(arr[i]);
+                ret = list.size()-1;
+            }
+            int pos = binary_search(list, 0, ret, arr[i]);
+            list.set(pos, arr[i]);
+        }
+        return ret + 1;
+    }
+
+    static int binary_search(List<Integer> list, int st, int end, int val){
+        while(st < end){
+            int mid = (st+end)/2;
+            if(val > list.get(mid)){
+                st = mid+1;
+            }else{
+                end = mid;
+            }
+        }
+        return end;
+    }
+}
